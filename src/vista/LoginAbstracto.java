602507faf33f86848f19d611ab7db11265013f6c
danie <danie@DESKTOP-3HH31E2>
2022-05-29 15:03:46 -0300

    Ajuste Login para usuarios ya ingresados

--- src/vista/LoginAbstracto.java
+++ src/vista/LoginAbstracto.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 import logica.UsuarioException;
 
 public abstract class LoginAbstracto extends javax.swing.JDialog implements IvistaLogin {
+    
     public LoginAbstracto(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
@@ -13,9 +14,9 @@ public abstract class LoginAbstracto extends javax.swing.JDialog implements Ivis
     }
 
     public abstract String getTitulo();
-
+    
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -25,7 +26,7 @@ public abstract class LoginAbstracto extends javax.swing.JDialog implements Ivis
         tfPassword = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("LOGIN");
+        setPreferredSize(new java.awt.Dimension(255, 200));
 
         jLabel1.setText("Usuario:");
 
@@ -50,21 +51,21 @@ public abstract class LoginAbstracto extends javax.swing.JDialog implements Ivis
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(56, 56, 56)
+                .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel2)
                     .addComponent(jLabel1))
                 .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(bLogin, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
-                    .addComponent(tfNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
-                    .addComponent(tfPassword))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(bLogin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(tfNombre)
+                    .addComponent(tfPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(45, 45, 45)
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(tfNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -74,28 +75,28 @@ public abstract class LoginAbstracto extends javax.swing.JDialog implements Ivis
                     .addComponent(tfPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(bLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(24, Short.MAX_VALUE))
+                .addContainerGap(57, Short.MAX_VALUE))
         );
 
         pack();
-    }// </editor-fold>                        
+    }// </editor-fold>//GEN-END:initComponents
 
-    private void bLoginActionPerformed(java.awt.event.ActionEvent evt) {                                       
+    private void bLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bLoginActionPerformed
         login();
-    }                                      
+    }//GEN-LAST:event_bLoginActionPerformed
 
-    private void tfPasswordActionPerformed(java.awt.event.ActionEvent evt) {                                           
+    private void tfPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfPasswordActionPerformed
         login();
-    }                                          
+    }//GEN-LAST:event_tfPasswordActionPerformed
 
 
-    // Variables declaration - do not modify                     
+    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bLogin;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField tfNombre;
     private javax.swing.JPasswordField tfPassword;
-    // End of variables declaration                   
+    // End of variables declaration//GEN-END:variables
 
     private void login() {
         String nom = tfNombre.getText();
